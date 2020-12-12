acc30d2d0cc30b85c030a685542453d3474d48fe
Weihan He <weihan@thirdinsight.ai>
2020-12-11 19:25:30 -0600

    first scoring agent

--- agent/player.py
+++ agent/player.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torchvision.transforms.functional as TF
+import torchvision
 import torch
 
 from .classifier import load_model as load_classifier
@@ -20,6 +21,7 @@ class HockeyPlayer:
        Call `python3 -c "import pystk; pystk.init(pystk.GraphicsConfig.ld()); print(pystk.list_karts())"` to see all values.
     """
     kart = ""
+    ball_share = [0,0,0]
     
     def __init__(self, player_id = 0):
         def get_goal(player_id):
@@ -44,6 +46,8 @@ class HockeyPlayer:
         self.player_id = player_id
         self.our_goal, self.our_mid_goal, self.enemy_goal, self.enemy_mid_goal = get_goal(self.player_id)
         self.last_ball_screen = [0,0]
+        self.ball_was_on_screen = True
+        self.step = 0
 
         # load model
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -59,7 +63,7 @@ class HockeyPlayer:
             proj = np.array(player.camera.projection).T
             view = np.array(player.camera.view).T
             p = proj @ view @ np.array(list(dest) + [1])
-            screen =  np.array([p[0] / p[-1], - p[1] / p[-1]])
+            screen =  np.array([p[0] / abs(p[-1]), - p[1] / p[-1]])
             return screen, p[-1] > 0
 
         def to_world(aim_point, player, height=0):
@@ -74,13 +78,17 @@ class HockeyPlayer:
                 t = 10
             return t * x1 + (1-t) * x0
 
-        def ball_in_front(ball_distance, ball_screen):
-            # return ball_distance<7 and abs(ball_screen[0]) < 0.15
-            return False
+        def ball_in_middle(ball_screen):
+            return abs(ball_screen[0]) < 0.15
 
-        def hit_ball(ball_screen, player, enemy_mid_goal):
-            goal_screen, _ = world_to_screen(player, enemy_mid_goal)
-            return ball_screen[0] - goal_screen[0]
+        def ball_in_front(ball_distance, ball_screen, speed):
+            return (ball_distance/speed) < 0.6 and ball_in_middle(ball_screen)
+            # return False
+
+        def hit_ball(ball_screen, player, enemy_goal):
+            left_goal_screen, _ = world_to_screen(player, enemy_goal[0])
+            right_goal_screen, _ = world_to_screen(player, enemy_goal[1])
+            return max(ball_screen[0] - left_goal_screen[0], ball_screen[0] - right_goal_screen[0])
 
         def clip_steer(steer):
             return min(max(steer, -1), 1)
@@ -101,27 +109,64 @@ class HockeyPlayer:
         """
         Your code here.
         """
-        ball_on_screen = self.classifier(TF.to_tensor(image)[None].to(self.device)).cpu().tolist()
-        if ball_on_screen[0][0] < ball_on_screen[0][1]:
+        speed = np.linalg.norm(player_info.kart.velocity)
+        classifier_output = self.classifier(TF.to_tensor(image)[None].to(self.device)).cpu().tolist()
+        ball_on_screen = classifier_output[0][0] < classifier_output[0][1]
+        
+        if ball_on_screen and not self.ball_was_on_screen:
+            self.ball_was_on_screen = True
+            ball_on_screen = False
+        else:
+            self.ball_was_on_screen = ball_on_screen
+
+        if ball_on_screen:
             o, _ = self.detector(TF.to_tensor(image)[None].to(self.device))
             ball_screen = o.cpu().squeeze().detach().numpy().tolist()
             self.last_ball_screen = ball_screen
             ball_world = to_world(ball_screen, player_info)
+            HockeyPlayer.ball_share = (ball_world, self.step)
             # print(ball_world)
             ball_distance = min(np.linalg.norm(np.array(player_info.kart.location) - np.array(ball_world)),30)
             # print(ball_distance)
-            action['acceleration'] = 1
-            if ball_in_front(ball_distance, ball_screen):
-                action['steer'] = clip_steer(hit_ball(ball_screen, player_info, self.enemy_mid_goal))
+            action['acceleration'] = 1 if ball_in_middle(ball_screen) else 0.6
+
+            if ball_in_front(ball_distance, ball_screen, speed):
+
+                action['steer'] = clip_steer(hit_ball(ball_screen, player_info, self.enemy_goal))
             else:
-                action['steer'] = clip_steer(ball_screen[0]*5)
-        else:
+                action['steer'] = clip_steer(ball_screen[0]*10)
+            
+            # with open(str(self.player_id) + ".txt", "a") as f:
+            #     f.write("step:%d, ball_distance:%0.2f, speed:%0.2f\n" % 
+            #         tuple([self.step, ball_distance, speed]))
+            #     f.write(str(action)+"\n")
+            #     f.close()
+        else:        
             action['acceleration'] = 0
             action['brake'] = True
             action['steer'] = -1 if self.last_ball_screen[0] > 0 else 1 
-            # action['steer'] = -1
-            # action['steer'] = -1 if np.random.uniform(-1,1) > 0 else 1
 
-        return action, ball_on_screen[0][0] < ball_on_screen[0][1], self.last_ball_screen.copy()
+            if HockeyPlayer.ball_share[1] - self.step < 2:
+                ball_screen, in_front = world_to_screen(player_info, HockeyPlayer.ball_share[0])
+                if in_front:
+                    action['acceleration'] = 1 if ball_in_middle(ball_screen) else 0.6
+                    action['steer'] = clip_steer(ball_screen[0]*10)
+                    action['brake'] = False
+
+            # with open(str(self.player_id) + ".txt", "a") as f:
+            #     f.write("step:%d, speed:%0.2f\n" % 
+            #         tuple([self.step, speed]))
+            #     f.write(str(action)+"\n")
+            #     f.close()
+        
+        if self.step < 30:
+            ball_screen, _ = world_to_screen( player_info, [-0.476, 0.37, 0.544])
+            action['steer'] = clip_steer(ball_screen[0]*10)
+            action['brake'] = False
+            action['acceleration'] = 1.0
+
+        self.step += 1
+
+        return action, ball_on_screen, self.last_ball_screen.copy()
 
    
